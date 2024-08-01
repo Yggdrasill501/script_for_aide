d8815d5da669200ff24ffc15bd3fb15506bf8977
Filip Žitný <fzitny@gpe.cz>
2024-08-01 16:38:34 +0200

    update

--- main.py
+++ main.py
@@ -1,36 +1,25 @@
-"""Main module handels exporting logs from AIDE."""
+"""Main module saves AIDE logs into path"""
 import os
 import csv
 import re
-import time
+from datetime import datetime
 
-# Define log directory and output CSV file
+LOG_DIR = "/var/log/remote/aide" # Define log directory and output CSV file
 LOG_DIR = "/var/log/remote/aide"
 CSV_DIR = "/path/to/csv"
-CSV_FILE = os.path.join(CSV_DIR, f"aide_logs_{str(counter)}.csv")
+CSV_FILE = os.path.join(CSV_DIR, f"aide_logs_{datetime.now().strftime('%Y%m%d')}.csv")
 
 # Define regex to extract required log data
 log_pattern = re.compile(r'(\w+ \d+ \d+:\d+:\d+) (\S+) aide: Start timestamp: (.+)')
 
 
-def counter() -> int:
-    """Counts plus one digit every day.
-    
-    :returns: int.
-    """
-    time.sleep(86400)
-    _counter: int = 0
-    _counter += 1
-    return _counter
-
-
-def extract_logs(log_dir) -> list:
+def extract_logs(log_dir: str, log_list: list) -> list:
     """Extracts logs from given diretory
 
     :param log_dir: str, path to directory.
+    :param log_list: list, list of logs.
     :returns: list
     """
-    logs = []
     for root, _, files in os.walk(log_dir):
         for file in files:
             file_path = os.path.join(root, file)
@@ -39,29 +28,30 @@ def extract_logs(log_dir) -> list:
                     match = log_pattern.search(line)
                     if match:
                         log_time, server, timestamp = match.groups()
-                        logs.append({
+                        log_list.append({
                             "log_time": log_time,
                             "server": server,
                             "timestamp": timestamp
                         })
-    return logs
+    return log_list
 
 
-def transform_to_csv(logs, csv_file) -> None:
+def transform_to_csv(log_list: list, csv_file: str) -> None:
     """Transforms logs to CSV format
 
-    :param logs: list, list of logs.
+    :param log_list: list, list of logs.
     :param csv_file: str, path to the csv file.
     :returns: None.
     """
     fieldnames = ["log_time", "server", "timestamp"]
-    with open(csv_file, 'w', newline='') as csvfile:
+    with open(csv_file, 'w', newline=' ') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        for log in logs:
+        for log in log_list:
             writer.writerow(log)
 
+LIST_OF_LOGS = []
 
 if __name__ == "__main__":
-    logs = extract_logs(LOG_DIR)
+    logs = extract_logs(LOG_DIR, log_list=LIST_OF_LOGS)
     transform_to_csv(logs, CSV_FILE)
