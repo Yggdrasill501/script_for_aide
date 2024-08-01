18ed3d8fad7457bcd2189bf3647223cddae2c57b
yggdrasill501 <fzitny@gpe.cz>
2024-08-01 09:22:28 +0200

    docstrings

--- main.py
+++ main.py
@@ -11,7 +11,13 @@ CSV_FILE = os.path.join(CSV_DIR, f"aide_logs_{datetime.now().strftime('%Y%m%d')}
 # Define regex to extract required log data
 log_pattern = re.compile(r'(\w+ \d+ \d+:\d+:\d+) (\S+) aide: Start timestamp: (.+)')
 
-def extract_logs(log_dir):
+
+def extract_logs(log_dir) -> list:
+    """Extracts logs from given diretory
+
+    :param log_dir: str, path to directory.
+    :returns: list
+    """
     logs = []
     for root, _, files in os.walk(log_dir):
         for file in files:
@@ -28,7 +34,14 @@ def extract_logs(log_dir):
                         })
     return logs
 
-def transform_to_csv(logs, csv_file):
+
+def transform_to_csv(logs, csv_file) -> None:
+    """Transforms logs to CSV format
+
+    :param logs: list, list of logs.
+    :param csv_file: str, path to the csv file.
+    :returns: None.
+    """
     fieldnames = ["log_time", "server", "timestamp"]
     with open(csv_file, 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -36,9 +49,7 @@ def transform_to_csv(logs, csv_file):
         for log in logs:
             writer.writerow(log)
 
-def main():
-    logs = extract_logs(LOG_DIR)
-    transform_to_csv(logs, CSV_FILE)
 
 if __name__ == "__main__":
-    main()
+    logs = extract_logs(LOG_DIR)
+    transform_to_csv(logs, CSV_FILE)
